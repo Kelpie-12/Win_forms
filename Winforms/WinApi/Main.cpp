f1d67eebd9463c1192f05bde778b28d553f604fc
Roman Yarkov <roman121998yarkov@gmail.com>
2024-07-29 16:27:28 +0400

    Copy text done

---
+++ Winforms/WinApi/Main.cpp
@@ -0,0 +1,55 @@
+#include <Windows.h>
+#include "resource.h"
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParan, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR ipCmdLine, INT nCmdShow)
+{
+	//MessageBox(NULL, "Hello World!\nСобщение", "Заголовок окна", MB_YESNOCANCEL| MB_ICONWARNING|MB_DEFBUTTON3);
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL DlgProc(HWND hwnd, UINT uMsg, WPARAM wParan, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG://отрабатывает только один раз, при создание окна
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		//HWND hEdinLogin = GetDlgItem(hwnd, IDC_EDIT_Login);
+		//SetFocus(hEdinLogin);
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_Login));
+	}
+	break;
+	case WM_COMMAND://Обрабатываются нажатия на кнопки, ввод текста и любые изменения состояния окна
+		switch (LOWORD(wParan))
+		{
+		case IDC_BUTTON_Copy:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_Login);
+			HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_Password);
+			CONST INT size = 256;
+			CHAR sz_buff[size]{};
+			SendMessage(hEdit, WM_GETTEXT, size, (LPARAM)sz_buff);
+			SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)sz_buff);
+		}
+		break;
+		case IDOK:
+			MessageBox(hwnd, "Была нажата ок", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_CLOSE://Обрабатываются при нажатие на кнопку Х (закрыть)
+		EndDialog(hwnd, 0);
+	default:
+		break;
+	}
+	return false;
+}
---
+++ Winforms/WinApi/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDI_ICON1                       103
+#define IDC_EDIT1                       1001
+#define IDC_EDIT_Password               1001
+#define IDC_STATIC_Login                1002
+#define IDC_STATIC_Password             1003
+#define IDC_EDIT_Login                  1004
+#define IDC_BUTTON_Copy                 1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
